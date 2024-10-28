6fca58b4b8559b23dbbc07018b01da2d178bd7e0
Łukasz Spasiński <01171680@pw.edu.pl>
2024-10-28 14:43:12 +0100

    zejsciowka lab1

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,21 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+
+private:
+    double x, y;
+
+public:
+    Wektor2D(double x, double y) : x(x), y(y) {}
+
+    // Gettery i settery
+    double getX() const { return x; }
+    double getY() const { return y; }
+    void   setX(double noweX) { x = noweX; }
+    void   setY(double noweY) { y = noweY; }
+
+    // Przeci¹¿enie operatora + (dodawanie wektorów)
+    Wektor2D operator+(const Wektor2D& nowe) const { return Wektor2D(x + nowe.x, y + nowe.y); }
+
+    // Przeci¹¿enie operator * (iloczyn skalarny)
+    double operator*(const Wektor2D& nowe) const { return x * nowe.x + y * nowe.y; }
 };
